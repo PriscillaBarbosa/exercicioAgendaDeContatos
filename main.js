5af0901e2306de8e917126c0cb477b04ab7808e7
PRISCILLA BÁRBOSA <priscillabarbosa@outlook.com>
2023-12-14 16:58:24 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("form-atividade");
+const nome = [];
+const telefone = [];
+
+
+let linhas = "";
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById("nome");
+    const inputTelefone = document.getElementById("telefone");
+
+    if(nome.includes(inputNome.value) || telefone.includes(inputTelefone.value) ){
+        alert(`O nome: ${inputNome.value} ou o telefone: ${inputTelefone.value} já foi cadastrado`);
+    }else {
+
+    nome.push(inputNome.value);
+    telefone.push(parseFloat(inputTelefone.value));
+
+    let linha = `<tr>`;
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+}
+
+    inputNome.value = "";
+    inputTelefone.value = "";
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector("tbody");
+    corpoTabela.innerHTML = linhas;
+}
